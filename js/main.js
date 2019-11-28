21ff3269cd6c3ffdf92055a8a06bfc32a3e29c6a
Damian Jackiewicz <damian.jackiewicz92@gmail.com>
2019-11-28 06:10:55 +0100

    Add flag to change transition

--- js/main.js
+++ js/main.js
@@ -21,9 +21,22 @@ function setTime() {
     hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
 
 
+    let flag = true;
+
+    // Set the flag validation for seconds
     if (secondsDegrees === 90) {
-        secondHand.style.transition = "none"
+        flag = !flag;
+    } else if (secondsDegrees > 90) {
+        flag = flag;
     }
+
+    // Set new transition when flag is true or false for seconds
+    if (flag) {
+        secondHand.style.transition = "all 0.05s cubic-bezier(0.1, 1.57, 0.13, 1.99)";
+    } else {
+        secondHand.style.transition = "none";
+    }
+
 }
 
 setInterval(setTime, 1000)
